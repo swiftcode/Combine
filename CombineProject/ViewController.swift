84139469ac940b2e793709c210151b0ca806d91d
swiftcode <swiftcode@users.noreply.github.com>
2024-11-03 12:54:31 -0600

    Initial Commit

---
+++ CombineProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CombineProject
+//
+//  Created by Michael Campbell on 11/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CombineProjectTests/CombineProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CombineProjectTests.swift
+//  CombineProjectTests
+//
+//  Created by Michael Campbell on 11/3/24.
+//
+
+import Testing
+@testable import CombineProject
+
+struct CombineProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
